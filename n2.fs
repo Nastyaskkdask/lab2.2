dbd116b549c234a188184d3fb3abf36279778a30
Nastyaskkdask <144699427+Nastyaskkdask@users.noreply.github.com>
2025-03-13 23:40:21 +0500

    n2.fs

--- n2.fs
+++ n2.fs
@@ -1,42 +1,40 @@
 open System
 
-
 let rnd = new Random()
 
 let generateRandomString (minL: int) (maxL: int) =
     let length = rnd.Next(minL, maxL + 1)
     let chars = Array.zeroCreate length
     for i in 0..length - 1 do
-        let charType = rnd.Next(3) 
+        let charType = rnd.Next(3)
         chars[i] <-
             match charType with
-            | 0 -> char (rnd.Next(10) + int '0') 
-            | 1 -> char (rnd.Next(26) + int 'a') 
+            | 0 -> char (rnd.Next(10) + int '0')
+            | 1 -> char (rnd.Next(26) + int 'a')
             | 2 -> char (rnd.Next(26) + int 'A')
             | _ -> ' '
     String chars
 
 let generateRand (count: int) (minL: int) (maxL: int) =
     [ for i in 1..count do
-        generateRandomString minL maxL ]
+          generateRandomString minL maxL ]
 
 let find (stringList: string list) =
     match stringList with
     | [] -> None
     | head :: tail ->
         let shortest =
-            List.fold (fun (acc : string) (str : string) ->
+            List.fold (fun (acc : string) (str: string) ->
                 if str.Length < acc.Length then
                     str
                 else
                     acc) head tail
         Some shortest
 
-
 printf "Выберите способ ввода списка строк (1 - рандомный, 2 - ручной): "
 let inputMode = Console.ReadLine()
 
-let MyList =
+let myList = 
     match inputMode with
     | "1" ->
         printf "Введите кол-во строк: "
@@ -51,22 +49,21 @@ let MyList =
             exit 1
     | "2" ->
         printf "Введите строки (каждую на новой строке, пустая строка - конец ввода):\n"
-        let mutable lines = []
-        let mutable line = Console.ReadLine()
-        while not (String.IsNullOrEmpty(line)) do
-            lines <- line :: lines
-            line <- Console.ReadLine()
-        List.rev lines 
+        let rec readLines acc =
+            match Console.ReadLine() with
+            | line when String.IsNullOrEmpty(line) -> List.rev acc
+            | line -> readLines (line :: acc)
+
+        readLines [] 
     | _ ->
         printfn "Ошибка: Некорректный выбор режима ввода."
         exit 1
 
 
 printfn "Сгенерированный список строк:"
-MyList |> List.iter (printfn "%s")
+myList |> List.iter (printfn "%s")
 
-match find MyList with
+match find myList with
 | Some shortestString -> printfn "\nСамая короткая строка: %s" shortestString
 | None -> printfn "\nСписок пуст."
-
 
